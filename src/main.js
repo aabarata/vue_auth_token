37fd4c63a3ab7b185a516826160055103881310f
André <2130869@my.ipleiria.pt>
2020-01-29 15:35:12 +0100

    Automatic login

--- src/main.js
+++ src/main.js
@@ -2,11 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
+import axios from 'axios'
+import { TokenExpiredError } from 'jsonwebtoken'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created() {
+    //Set the user stored locally in the store state
+    const userString = localStorage.getItem('user')
+    if(userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData);
+    }
+    //Security mesure to prevent setting user with fake token manually at the local storage
+    axios.interceptors.response.use(
+      response => response, 
+      error => {
+        if(error.response.status === 401) {
+          this.$store.dispatch('logout')
+        }
+        return Promise.reject(error)
+      }
+    )
+  },
   render: h => h(App)
 }).$mount('#app')
